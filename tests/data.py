244ef7f5aadaaef6879301718b4c8ef715475011
Tatiana Yurchenko <taf88156@mail.ru>
2024-04-03 16:58:07 +0300

    update: refactor test_lesson_1.py

---
+++ tests/data.py
@@ -0,0 +1,19 @@
+# AUTH
+
+LOGIN = 'standard_user'
+PASSWORD = 'secret_sauce'
+LOGIN_NEGATIVE = 'user'
+PASSWORD_NEGATIVE = 'user'
+
+# CHECKOUT
+FERST_NAME = 'Tatiana'
+LAST_NAME = 'Tatiana'
+ZIP = '1236'
+# URLS
+
+URL_LOGIN_PAGE = "https://www.saucedemo.com/"
+URL_INVENTORY_PAGE = "https://www.saucedemo.com/inventory.html"
+URL_CARD_PAGE = "https://www.saucedemo.com/cart.html"
+URL_CHECKOUT_PAGE = "https://www.saucedemo.com/checkout-step-one.html"
+URL_CHECKOUT_PAGE_STEP_TWO = "https://www.saucedemo.com/checkout-step-two.html"
+URL_ABOUT_PAGE = "https://saucelabs.com/"
